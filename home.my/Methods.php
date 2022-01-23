c0f68d6674afed978debeeb2ac904433eccf05f1
Andrisssd <88433887+Andrisssd@users.noreply.github.com>
2022-01-24 00:21:24 +0200

    Add files via upload

---
+++ home.my/Methods.php
@@ -0,0 +1,67 @@
+<?php
+namespace Methods
+{
+
+    class ArrayManipulator
+    {
+        public static function GetNumbersFrom0To($number)
+        {
+            $numberArray = [];
+            for ($i = 1; $i <= $number; $i++) {
+                array_push($numberArray, $i);
+            }
+
+            return $numberArray;
+        }
+
+        public static function GetPrimeNumberArrayFrom($array)
+        {
+            $primeNumberArray = [];
+            for ($i = 0; $i < count($array); $i++) {
+                $number = $array[$i];
+                $numbersFilterBy = [2, 3, 5, 7];
+                $isPrime = true;
+                for ($j = 0; $j < count($numbersFilterBy); $j++) {
+                    $numberFilterBy = $numbersFilterBy[$j];
+                    if ($number % $numberFilterBy == 0 && $number != $numberFilterBy) {
+                        $isPrime = false;
+                    }
+                }
+
+                if ($isPrime) {
+                    array_push($primeNumberArray, $number);
+                }
+            }
+
+            return $primeNumberArray;
+        }
+
+        public static function GetPalindromeNumbersFrom($array)
+        {
+            $palindromeArray = [];
+            foreach ($array as $number) {
+                $numberStr = strval($number);
+                $reversedNumberStr = strrev($numberStr);
+                if ($numberStr == $reversedNumberStr) {
+                    array_push($palindromeArray, $number);
+                }
+            }
+
+            return $palindromeArray;
+        }
+
+        public static function GetGeneratedPasswordFrom($numberArray, $passwordLength)
+        {
+            $passwordStr = "";
+            while (strlen($passwordStr) < $passwordLength) {
+                $index = rand(0, count($numberArray) - 1);
+                $numberStr = strval($numberArray[$index]);
+                if (strlen($numberStr) + strlen($passwordStr) <= $passwordLength) {
+                    $passwordStr .= $numberStr;
+                }
+            }
+
+            return $passwordStr;
+        }
+    }
+}
---
+++ home.my/index.php
@@ -0,0 +1,25 @@
+<?php
+namespace Program
+{
+    require 'Methods.php';
+
+    $numberTotalCount = $argv[1];
+    $passwordMaxLength = $argv[2];
+    define('passwordMinLength', 3);
+    $passwordMaxLengthIsValid = $passwordMaxLength >= passwordMinLength;
+
+    if ($passwordMaxLengthIsValid) {
+        $numberArray = \Methods\ArrayManipulator::GetNumbersFrom0To($numberTotalCount);
+        $primeNumberArray = \Methods\ArrayManipulator::GetPrimeNumberArrayFrom($numberArray);
+        $palindromeNumberArray = \Methods\ArrayManipulator::GetPalindromeNumbersFrom($numberArray);
+        $passwordLength = rand(passwordMinLength, $passwordMaxLength);
+        $password = \Methods\ArrayManipulator::GetGeneratedPasswordFrom($primeNumberArray, $passwordLength);
+
+        echo "Prime numbers : " . join(" ", $primeNumberArray), PHP_EOL;
+        echo "Palindrome numbers : " . join(" ", $palindromeNumberArray), PHP_EOL;
+        echo "Password : " . $password, PHP_EOL;
+    } else {
+        echo "Password length can't be less than 3 symbols";
+    }
+}
+
